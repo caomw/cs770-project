7e72c909896a01831955730ccf7bbc84eef20bc9
Chris <cbodden@cs.wisc.edu>
2015-10-20 01:00:30 -0500

    hand tracker system commit
    requires Kinect for Windows SDK 2.0

---
+++ HandTracker/resource.h
@@ -0,0 +1,32 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by HandTracker.rc
+//
+#define IDI_APP                         100
+#define IDD_APP                         100
+#define IDR_MENU1                       101
+#define IDD_FORMVIEW                    101
+#define IDC_VIDEOVIEW                   1000
+#define IDC_STATUS                      1001
+#define IDC_IP                          1002
+#define IDC_SAVE                        1003
+#define IDC_CANCEL                      1004
+#define IDC_PORT                        1005
+#define ID_NETWORKSETTINGS              32777
+#define ID_CONROLHAND_RIGHT             32778
+#define ID_CONROLHAND_LEFT              32779
+#define ID_START                        32782
+#define ID_END                          32783
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         32784
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cs770-project
 HCI project implementation
+
+## HandTracker
+Kinect arm tracker (requires Kinect for Windows SDK 2.0)
